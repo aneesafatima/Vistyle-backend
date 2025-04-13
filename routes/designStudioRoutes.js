201942f17e66154549297490b3dc84967645179e
aneesa04 <aneesafatima238@gmail.com>
2025-04-13 11:45:34 +0530

    perf: changed the type of request for image processing

--- routes/designStudioRoutes.js
+++ routes/designStudioRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const designStudioController = require("../controllers/designStudioController");
 
 const router = express.Router();
-router.post(
-  "/create-item-sticker",
-  designStudioController.createItemSticker
+router.get(
+  "/create-item-mask",
+  designStudioController.createItemMask
 );
 module.exports = router;
