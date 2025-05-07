961c97a86b35ff7f82e63af8e00efa2cadcf5d5c
aneesa04 <aneesafatima238@gmail.com>
2025-05-07 16:05:27 +0530

    fix: fixing server bug

--- removeBg.py
+++ removeBg.py
@@ -1,27 +1,30 @@
+from rembg import remove
 import sys
 
 print("Script started")
 print("Received path:", sys.argv[1])
-sys.exit(0)
-# from rembg import remove
-# import sys
 
-# def remove_background(image):
-#     # Decode the base64 image
-#     # image_data = base64.b64decode(base64_image)
-#     print("In rembg function")
-#     try:
-#         with open(image, 'rb') as image_file:
-#             image_data = image_file.read()
-#             print("Data converted to bytes")
-#             # Remove the background
-#             output_image = remove(image_data)
-#             with open(image, 'wb') as output_file:
-#                  output_file.write(output_image)
-#     except Exception as e:
-#         sys.stderr.write(f"Error: {e}")
-#         sys.exit(1)
-#         print(f"Error: {e}")
+
+def remove_background(image):
+    # Decode the base64 image
+    # image_data = base64.b64decode(base64_image)
+    print("In rembg function")
+    try:
+        with open(image, 'rb') as image_file:
+            image_data = image_file.read()
+            print("Data converted to bytes")
+            # Remove the background
+            output_image = remove(image_data)
+            with open(image, 'wb') as output_file:
+                 output_file.write(output_image)
+    except Exception as e:
+        sys.stderr.write(f"Error: {e}")
+        print(f"Error: {e}")
+        sys.exit(1)
+
+remove_background(sys.argv[1])
+sys.exit(0)
+
   
 #     # Encode the output image to base64
 #     # result = base64.b64encode(output_image).decode('utf-8')
